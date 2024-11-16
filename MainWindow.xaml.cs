57c332c687776530b8205b8b7c6e4884ab81e3ef
anis-muhammad <164650683+anis-muhammad@users.noreply.github.com>
2024-11-17 01:50:49 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BC230200812
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double height = double.Parse(txtHeight.Text);
+                double weight = double.Parse(txtWeight.Text);
+
+                height = height / 100;
+
+                double bmi = weight / (height * height);
+
+                txtBMI.Text = Math.Round(bmi).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please enter valid Numbers for height and weight.",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void txtBMI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
